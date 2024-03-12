f1a956e94c75af95af2df5d1c7fda3d317f21051
Александр Рубцов <89102331+ACM-REZ@users.noreply.github.com>
2024-03-12 16:32:37 +0300

    env

--- src/reducers/weather/actions.js
+++ src/reducers/weather/actions.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import { changeCity } from "./reducer";
 
 export const fetchCity = (city) => {
-    return async (dispatch) => {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54e35a21935f39d6064c07547321781f`);
-        dispatch(changeCity(response.data));
-    }
-}
+  return async (dispatch) => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_TOKEN}`
+    );
+    dispatch(changeCity(response.data));
+  };
+};
